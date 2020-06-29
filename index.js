888daef07a0806c9980e63d0be8a9ca4d83928b3
saqibYousuf <saqibyousuf001@gmail.com>
2020-06-29 22:05:49 +0500

    some changes on postback fn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.post('/webhook', (req, res) => {
       // Gets the message. entry.messaging is an array, but 
       // will only ever contain one message, so we get index 0
       let webhook_event = entry.messaging[0];
-      let PSID = webhook_event.recipient.id
+      let PSID = webhook_event.sender.id
       if(PSID){
         postBack(PSID)
       }
